4781dacc0b3b73e21c2ba0d3d454d0e406959e35
BardAlexandrovskiy <alexandr8558@gmail.com>
2019-08-18 19:15:37 +0300

    bugs fixed

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,5 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-param-reassign */
 /* eslint-disable arrow-parens */
 /* eslint-disable no-console */
