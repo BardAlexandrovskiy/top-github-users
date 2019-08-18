9c0827fa7bf565565cf495e71680b3487b908a22
BardAlexandrovskiy <alexandr8558@gmail.com>
2019-08-18 19:26:29 +0300

    Updates

---
+++ precache-manifest.e1b099e8276ecdb2e199f19bd922f506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c7f28347db28bcc8e445576dda24d3",
+    "url": "/top-github-users/index.html"
+  },
+  {
+    "revision": "34c319f0cfed58de8561",
+    "url": "/top-github-users/static/css/main.803d8f9a.chunk.css"
+  },
+  {
+    "revision": "54597c29ad08bc420ada",
+    "url": "/top-github-users/static/js/2.3d258b33.chunk.js"
+  },
+  {
+    "revision": "34c319f0cfed58de8561",
+    "url": "/top-github-users/static/js/main.b8c64367.chunk.js"
+  },
+  {
+    "revision": "15897eda16b32640c8cf",
+    "url": "/top-github-users/static/js/runtime~main.33efc2c0.js"
+  }
+]);
